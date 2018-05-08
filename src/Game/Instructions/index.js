9b837bdb76ec4ecbbf0708591a90df2bf0644e3f
Timothy Jones <tjones@dius.com.au>
2018-05-08 16:57:06 +1000

    Further clarify rules

--- src/Game/Instructions/index.js
+++ src/Game/Instructions/index.js
@@ -9,14 +9,17 @@ export default class ScoreBoard extends React.Component<Props> {
     return (
       <div>
         <h1>PlusOne</h1>
-        <p>Your score is the highest number in the square.</p>
+        <p>
+          The cell with the highest number is white. This is your score, and
+          bigger is better.
+        </p>
         <p>
           You can merge cells that share the same number as long as they're
           touching. Merged cells increase their number by one.
         </p>
         <p>
-          The highest numbered cell is white. Each time you merge white cells,
-          the lowest numbered cells also fall off the board.
+          Bonus: Each time you merge white cells, the lowest numbered cells also
+          fall off the board.
         </p>
         <p>Good luck!</p>
       </div>
