066fa8a9aa9e3120019a241cb6ea75fed59695eb
Timothy Jones <tjones@dius.com.au>
2018-08-10 10:49:05 +1000

    Use prerelease version for pact too

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,13 +1,13 @@
-let publisher = require('@pact-foundation/pact-node');
-let path = require('path');
-let pack = require('../package.json')
+const publisher = require('@pact-foundation/pact-node');
+const path = require('path');
+const gitRevision = require('git-revision-webpack-plugin');
 
 let opts = {
   pactFilesOrDirs: [path.resolve(process.cwd(), 'pacts')],
   pactBroker: process.env.PACT_BROKER_HOST,
   pactBrokerUsername: process.env.PACT_BROKER_USERNAME,
   pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
-  consumerVersion: pack.version
+  consumerVersion: (new gitRevision()).version()
 };
 
 publisher.publishPacts(opts).then(() => done());
